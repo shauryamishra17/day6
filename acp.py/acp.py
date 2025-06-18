28d6f37314605bcebf2acfd45759b954a04b90db
shauryamishra17 <madaraboy776@gmail.com>
2025-06-18 16:56:56 +0530

    day6

---
+++ acp.py/acp.py
@@ -0,0 +1,7 @@
+c=(input("Enter the character:"))
+if len(c)  !=1:
+    print("Eror....only put one character")
+elif c.isalpha():
+    print(c,"Is an alphabat.")
+else:
+    print(c,"is not an alphabhat.")
